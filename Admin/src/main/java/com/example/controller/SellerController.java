b35f633e08e325940f804136245dc62e04896ee1
minji8431 <minji8431@naver.com>
2022-05-30 10:21:50 +0900

    5/30
    업체페이지올림

---
+++ Admin/src/main/java/com/example/controller/SellerController.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Sellerid;
+import com.example.service.SellerService;
+
+@Controller
+public class SellerController {
+
+	static final Logger logger = LoggerFactory.getLogger(SellerController.class);
+	
+	@Autowired
+	private SellerService sellerService;
+	
+	@RequestMapping("/product-details")
+	public void getproduct() {
+	}
+	
+	@RequestMapping("/shopInfo")
+	public void getmyshopInfo() {
+	}
+	
+	@RequestMapping("/shopInfoMod")
+	public void getshopInfoMod(Sellerid vo, Model m) {
+		logger.info("게시물 상세보기");
+		Sellerid vo1 = sellerService.selectshopInfo(vo);
+		m.addAttribute("Seller",vo1);
+	}
+	
+	@RequestMapping("/shopProView")
+	public void getmyproduct() {
+	}
+	
+	@RequestMapping("/shopProReg")
+	public void getshopProReg() {
+	}
+	
+	@RequestMapping("/shopReview")
+	public void getshopReview() {
+	}
+}
