66ecf0626d4a0336d44710c7bf072c1fa7ba9c39
hulakim <99947499+hulakim@users.noreply.github.com>
2022-05-30 09:57:37 +0900

    2022/05/30
    ss

--- Admin/src/main/java/com/example/service/NoticeServiceImpl.java
+++ Admin/src/main/java/com/example/service/NoticeServiceImpl.java
@@ -1,17 +1,26 @@
 package com.example.service;
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.controller.AdminController;
 import com.example.domain.Notice;
+import com.example.persistence.ManagerRepository;
 import com.example.persistence.NoticeRepository;
 
 @Service
 public class NoticeServiceImpl implements NoticeService{
+	
+	static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
 	@Autowired
 	private NoticeRepository notiRepo;
+	
+	@Autowired
+	private ManagerRepository manaRepo;
 	
 	@Override// 공지사항 리스트 조회
 	public List<Notice> getNoticeList(Notice vo){
@@ -26,7 +35,15 @@ public class NoticeServiceImpl implements NoticeService{
 	
 	@Override//  공지사항 상세조회
 	public Notice getNotice(Notice vo) {
+		logger.info("serviceimpl noticecode:"+vo.getNoticecode());
 		return notiRepo.findById(vo.getNoticecode()).get();
+	}
+	
+	//삭제
+	@Override
+	public void deleteNotice(Notice vo) {
+		notiRepo.delete(vo);
+		logger.info("serviceimpl:"+vo.getNtitle());
 	}
 	
 	//수정
@@ -39,5 +56,5 @@ public class NoticeServiceImpl implements NoticeService{
 		
 		notiRepo.save(vv);
 	}
-	//삭제
+	
 }
