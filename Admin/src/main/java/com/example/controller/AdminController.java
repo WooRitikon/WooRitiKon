6a23d45e687e16ad91fb15c182b580fc57909a4c
hulakim <99947499+hulakim@users.noreply.github.com>
2022-06-03 13:02:09 +0900

    6/3

--- Admin/src/main/java/com/example/controller/AdminController.java
+++ Admin/src/main/java/com/example/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.chating.vo.Room;
 import com.example.domain.Manager;
 import com.example.domain.Notice;
 import com.example.service.NoticeService;
@@ -73,5 +80,74 @@ public class AdminController {
 		notiservice.deleteNotice(vo);
 		logger.info("Controller delete:"+vo.getNtitle());
 		return "redirect:getNoticeList";
+	}
+	
+	//채팅
+	List<Room> roomList = new ArrayList<Room>();
+	static int roomNumber = 0;
+	
+	@RequestMapping("/chat")
+	public ModelAndView chat() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("chat");
+		return mv;
+	}
+	
+	/**
+	 * 방 페이지
+	 * @return
+	 */
+	@RequestMapping("/room")
+	public ModelAndView room() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("room");
+		return mv;
+	}
+	
+	/**
+	 * 방 생성하기
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/createRoom")
+	public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){
+		String roomName = (String) params.get("roomName");
+		if(roomName != null && !roomName.trim().equals("")) {
+			Room room = new Room();
+			room.setRoomNumber(++roomNumber);
+			room.setRoomName(roomName);
+			roomList.add(room);
+		}
+		return roomList;
+	}
+	
+	/**
+	 * 방 정보가져오기
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/getRoom")
+	public @ResponseBody List<Room> getRoom(@RequestParam HashMap<Object, Object> params){
+		return roomList;
+	}
+	
+	/**
+	 * 채팅방
+	 * @return
+	 */
+	@RequestMapping("/moveChating")
+	public ModelAndView chating(@RequestParam HashMap<Object, Object> params) {
+		ModelAndView mv = new ModelAndView();
+		int roomNumber = Integer.parseInt((String) params.get("roomNumber"));
+		
+		List<Room> new_list = roomList.stream().filter(o->o.getRoomNumber()==roomNumber).collect(Collectors.toList());
+		if(new_list != null && new_list.size() > 0) {
+			mv.addObject("roomName", params.get("roomName"));
+			mv.addObject("roomNumber", params.get("roomNumber"));
+			mv.setViewName("chat");
+		}else {
+			mv.setViewName("room");
+		}
+		return mv;
 	}
 }
